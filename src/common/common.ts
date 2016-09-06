c3f75c38f8455ae1cfb84ddce44699547f226984
xega <xxegax@gmail.com>
2016-09-06 14:12:26 +0300

    function clamp to common/common.ts

--- src/common/common.ts
+++ src/common/common.ts
@@ -1,3 +1,7 @@
 export function className(...args: Array<string | boolean>): string {
   return args.filter(name => typeof name == 'string').map(name => name).join(' ');
 }
+
+export function clamp(value: number, minMax: Array<number>) {
+  return Math.min(Math.max(value, minMax[0]), minMax[1]);
+}
