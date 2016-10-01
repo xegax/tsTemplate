79a10be9c85aac9a3949883c4f34f2cba26bb145
xega <xxegax@gmail.com>
2016-10-01 23:29:12 +0300

    removed example-2.tsx

--- src/examples/example-2.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import {Control} from 'controls/control';
-
-let cont = document.createElement('div');
-document.body.appendChild(cont);
-
-ReactDOM.render(<Control text='Other'/>, cont);
